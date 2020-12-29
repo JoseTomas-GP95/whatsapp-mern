219b890ba3881b01c40ab7a62a0f1168f6a42c05
Jose Gonzalez <tomas.didimo1407@gmail.com>
2020-12-29 19:46:28 -0300

    Limpieza en archivos y estilo

---
+++ clon-whatsapp/src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.js';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Hola Soy el Sidebar</h3>
+    </div>
+  )
+}
+
+export default Sidebar;
