0c1f3e8cafce343403e407e1f50dbc3b78dd20ea
Jose Gonzalez <tomas.didimo1407@gmail.com>
2020-12-29 18:24:24 -0300

    Inicializar proyecto y firebase

---
+++ clon-whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkPi_UCwCgVu8JBVlht-7DhCMghqL_uzg",
+  authDomain: "whats-app-clone-76341.firebaseapp.com",
+  projectId: "whats-app-clone-76341",
+  storageBucket: "whats-app-clone-76341.appspot.com",
+  messagingSenderId: "231461786021",
+  appId: "1:231461786021:web:1739ccbf71e0ca3847584c",
+  measurementId: "G-PJLEGTKTMP"
+};
