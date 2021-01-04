0577aaebbdcc19a93bb65605c3a5b92bfb6511bc
Jose Gonzalez <tomas.didimo1407@gmail.com>
2021-01-04 10:49:19 -0300

    Footer y formulario

--- clon-whatsapp/src/Chat.js
+++ clon-whatsapp/src/Chat.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 
 function Chat() {
   const [codigo, actualizarCodigo] = useState("");
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     actualizarCodigo(Math.floor(Math.random() * 5000));
   }, []);
+
+  // Funcion que envia mensaje
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("Tu enviaste", input)
+
+    // Limpiar el input
+    setInput("");
+  };
+
 
   return (
     <div className="chat">
@@ -36,19 +49,33 @@ function Chat() {
       </div>
 
       <div className="chat__body">
-        <p className="chat__message">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
           <span className="chat__name">Tomás Didimo</span>Maria abajame el arroz
+          <span className="chat__timestamp">09:00am</span>
         </p>
         <p className="chat__message">
           <span className="chat__name">Tomás Didimo</span>
           Porfa que se va a quemar 🔥🔥🔥
+          <span className="chat__timestamp">09:05am</span>
         </p>
       </div>
 
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={ input }
+            type="text"
+            name = "prueba"
+            onChange={ (e) => setInput(e.target.value) }
+            placeholder="Escriba un mensaje"
+          />
+          <button onClick = { sendMessage } type = "submit" >Enviar Mensaje</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
 
 export default Chat;
-// 1:38:36
